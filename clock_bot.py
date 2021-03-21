5cfa0333306e5a7e7e882300b048f1aada7e9230
profileid <profileid@protonmail.com>
2021-03-21 17:08:38 +0000

    change time

--- clock_bot.py
+++ clock_bot.py
@@ -1,17 +1,38 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 from os import getenv
+from datetime import datetime
 
 load_dotenv()
 token = getenv('TOKEN')
-channel_name = getenv('CHANNEL_ID')
+channel_id = getenv('CHANNEL_ID')
 
 bot = commands.Bot(command_prefix='?')
 
 
+def get_emoji(hour: str) -> str:
+    pass
+
+
+def get_time_string() -> str:
+    time = datetime.now().strftime('%H:%M:%S')
+    return time
+
+
+@tasks.loop(seconds=50)
+async def change_time() -> None:
+    string = get_time_string()
+    channel = bot.get_channel(int(channel_id))
+    try:
+        print(f'Chaning channel name to {string}')
+        await channel.edit(name=string)
+    except Exception as e:
+        print(e)
+
+
 @bot.event
 async def on_ready():
+    change_time.start()
     print(f'Bot runs as {bot.user.name}')
-
 
 bot.run(token)
